14847d1c4aa1a41c3fc6a23c0ef07994334cf47b
Vinicius Guterres <viniguter@gmail.com>
2020-05-04 22:25:31 -0300

    changing painel front end

--- painel/index.php
+++ painel/index.php
@@ -34,18 +34,37 @@ if (isset($_POST['id_membro'])) {
     <title>Painel CMS </title>
 </head>
 <body>
+<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
 
-  <!-- header -->
-  <!-- <main role="main" class="container-md-9 ml-sm-auto col-lg-10 px-5 " >
-    <header class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-2 shadow">
-      <h2 class="navbar-brand col-sm-3 col-md-2 mr-0">Team-Workers</h2>
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </header>
-  </main> -->
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+<!-- Header -->
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="#">Sign out</a>
+    </li>
+  </ul>
+</nav>
+
+
+
 
   <!-- worker register (needs some ajust at front end card ) -->
   <div class="card container mt-5 ">
@@ -116,6 +135,8 @@ if (isset($_POST['id_membro'])) {
       </table>
     </div>
   </main>
+
+  
 
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
